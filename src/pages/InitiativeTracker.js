6c9e8c9fa6a13678c78a7785cb2dd0e1d92dcbd3
James Ballard <ballardj77@gmail.com>
2022-04-05 16:05:17 -0500

    initiative tracker page correct displayed characters in order of initiative

--- src/pages/InitiativeTracker.js
+++ src/pages/InitiativeTracker.js
@@ -1,21 +1,65 @@
 import { useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { Container } from "@mui/material";
+import { Container, Box, Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
 import rollInitiative from "../utilities/rollInitiative";
 
 export default function IniativeTracker() {
     const { state } = useLocation();
     const [encounter, setEncounter] = useState(state);
 
-    useEffect (() => {
-        console.log(encounter.characters)
-        encounter.participants = rollInitiative(encounter.characters);
-        console.log(encounter)
-    }, [encounter])
+    encounter.participants = rollInitiative(encounter.characters)
+
+    const tableBody = encounter.participants ? (
+        <TableBody>
+            {encounter.participants.map((participant) => (
+                <TableRow
+                    key={participant.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <TableCell component="th" scope="row">
+                        {participant.name}
+                    </TableCell>
+                    <TableCell align="right">{participant.armorClass}</TableCell>
+                    <TableCell align="right">{participant.initiativeMod}</TableCell>
+                    <TableCell align="right">{participant.initiative}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    ) : (
+        <TableBody>
+            <TableRow>
+            <TableCell align="right">Loading...</TableCell>
+            </TableRow>
+        </TableBody>
+    )
 
     return (
-        <Container component= "main" maxWidth="sm">
-            <h1>The encounter name is {encounter.name}</h1>
+        <Container component = "main" maxWidth="sm">
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1">
+                    {encounter.name}
+                </Typography>
+            </Box>
+            <TableContainer component="main">
+                <Table size="small" aria-label="a dense table"> 
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell align="right">AC</TableCell>
+                            <TableCell align="right">Initiative Mod</TableCell>
+                            <TableCell align="right">Initiative</TableCell>
+                        </TableRow>   
+                    </TableHead>
+                    {tableBody}
+                </Table>
+            </TableContainer>
         </Container>   
     )
 }
--- src/utilities/rollInitiative.js
+++ src/utilities/rollInitiative.js
@@ -1,5 +1,7 @@
 import rollD20 from "./rollD20";
 
+
+//TODO: add case for ties
 const rollInitiative = (participants) => {
     participants.forEach(participant => {
         participant.initiative = rollD20() + participant.initiativeMod
