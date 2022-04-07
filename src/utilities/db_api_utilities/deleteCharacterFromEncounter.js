9fa792bed7ad623d013c05bb88e64bbfa532f408
James Ballard <ballardj77@gmail.com>
2022-04-06 23:21:56 -0500

    fixed final linting errors. 2 problematic deleteCharacterFromEncounter and deleteMonsterFromEncounter refactored to a better form

--- src/utilities/db_api_utilities/deleteCharacterFromEncounter.js
+++ src/utilities/db_api_utilities/deleteCharacterFromEncounter.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const deleteCharacterFromEncounter = async (characterId, encounter) => {
-  const updatedCharacters = encounter.characters.filter(c => c.id != characterId)
+  const updatedCharacters = encounter.characters.filter(c => Number(c.id) !== Number(characterId))
   encounter.characters = updatedCharacters
   return await (await axios.post('http://localhost:8080/api/encounter/deleteCharacter', encounter)).data
 }
--- src/utilities/db_api_utilities/deleteMonsterFromEncounter.js
+++ src/utilities/db_api_utilities/deleteMonsterFromEncounter.js
@@ -3,11 +3,7 @@ import axios from 'axios'
 const deleteMonsterFromEncounter = async (index, encounter) => {
   const deleteIndex = Number(index) + 1
   const urlsArr = encounter.monsters.split(',')
-  const newUrlsArr = urlsArr.filter((u, index) => {
-    if (index !== deleteIndex) {
-      return u
-    }
-  })
+  const newUrlsArr = urlsArr.filter((u, index) => index !== deleteIndex)
   encounter.monsters = newUrlsArr.join(',')
   return await (await axios.post('http://localhost:8080/api/encounter/deleteMonster', encounter)).data
 }
